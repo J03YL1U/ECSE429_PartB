cc7fcd67dc9f97d24fdb9698b1704c7701af2e86
Ke Yan <ke.yan@mail.mcgill.ca>
2023-11-11 20:03:53 -0500

    update category desc

--- src/test/java/ECSE429_PartB/StepDefinitionsCategories.java
+++ src/test/java/ECSE429_PartB/StepDefinitionsCategories.java
@@ -17,13 +17,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @TestMethodOrder(MethodOrderer.Random.class)
 public class StepDefinitionsCategories {
     static OkHttpClient client = new OkHttpClient();
-    private String existingTitle;
+    private String existingId;
     private JSONArray errorMessage;
     private String idNewCategory;
 
 
+    //    ID006: Create a category
     @Given("a list of categories {string}")
-    public void aListOfCategories(String title) throws Exception{
+    public void aListOfCategories(String title) throws Exception {
         JSONObject obj = new JSONObject();
 
         obj.put("title", title);
@@ -42,11 +43,11 @@ public class StepDefinitionsCategories {
         JSONParser parser = new JSONParser();
         JSONObject responseJson = (JSONObject) parser.parse(responseBody);
 
-        this.existingTitle = (String) responseJson.get("id");
+        this.existingId = (String) responseJson.get("id");
     }
 
     @When("I create the category {string}")
-    public void iCreateTheCategoryTask(String title) throws Exception{
+    public void iCreateTheCategoryTask(String title) throws Exception {
         JSONObject obj = new JSONObject();
 
         obj.put("title", title);
@@ -70,9 +71,9 @@ public class StepDefinitionsCategories {
 
 
     @Then("the expected category status code received from the system is {string}")
-    public void theExpectedCategoryStatusCodeReceivedFromTheSystemIs(String statusCode) throws Exception{
+    public void theExpectedCategoryStatusCodeReceivedFromTheSystemIs(String statusCode) throws Exception {
         Request request = new Request.Builder()
-                .url("http://localhost:4567/categories/"+idNewCategory)
+                .url("http://localhost:4567/categories/" + idNewCategory)
                 .get()
                 .build();
 
@@ -97,7 +98,7 @@ public class StepDefinitionsCategories {
     }
 
     @And("{string} category will be in the list of category")
-    public void categoryWillBeInTheListOfCategoryTasks(String newCategory) throws Exception{
+    public void categoryWillBeInTheListOfCategoryTasks(String newCategory) throws Exception {
         Request request = new Request.Builder()
                 .url("http://localhost:4567/categories")
                 .get()
@@ -117,11 +118,110 @@ public class StepDefinitionsCategories {
         for (Object todoObject : todos) {
             JSONObject todo = (JSONObject) todoObject;
             String title = (String) todo.get("title");
-            if(title.equals(newCategory)){
+            if (title.equals(newCategory)) {
                 CategoryAdded = true;
             }
         }
 
         assertTrue(CategoryAdded);
     }
+
+    //    ID007: Update a category's description
+
+    @Given("an existing category {string} with description {string}")
+    public void anExistingCategoryWithDescription(String title, String desc) throws Exception {
+        JSONObject obj = new JSONObject();
+
+        obj.put("title", title);
+        obj.put("description", desc);
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/categories")
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+
+        this.existingId = (String) responseJson.get("id");
+    }
+
+    @When("I update the existing description to {string}")
+    public void iUpdateTheExistingDescriptionTo(String newDesc) throws Exception {
+        JSONObject obj = new JSONObject();
+
+        obj.put("description", newDesc);
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/categories/" + this.existingId)
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+
+        this.errorMessage = (JSONArray) responseJson.get("errorMessages");
+        this.idNewCategory = (String) responseJson.get("id");
+    }
+
+    @And("the existing category's description should be {string}")
+    public void theExistingCategorySDescriptionShouldBe(String newDesc) throws Exception {
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/categories/" + this.existingId)
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        String responseBody = response.body().string();
+
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+
+        JSONObject jsonObject = new JSONObject(responseJson);
+        JSONArray categories = (JSONArray) jsonObject.get("categories");
+
+        for (Object categoriesObj : categories) {
+            JSONObject todo = (JSONObject) categoriesObj;
+            String desc = (String) todo.get("description");
+            assertEquals(newDesc, desc);
+        }
+
+    }
+
+    @When("I update the description to {string} of a category that doesn't exist")
+    public void iUpdateTheDescriptionToOfACategoryThatDoesnTExist(String newDesc) throws Exception {
+        JSONObject obj = new JSONObject();
+
+        obj.put("description", newDesc);
+        String invalidId = "1234567890";
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), obj.toString());
+
+        Request request = new Request.Builder()
+                .url("http://localhost:4567/categories/" + invalidId)
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+        JSONParser parser = new JSONParser();
+        JSONObject responseJson = (JSONObject) parser.parse(responseBody);
+
+        this.errorMessage = (JSONArray) responseJson.get("errorMessages");
+        this.idNewCategory = (String) responseJson.get("id");
+    }
+
+
 }
---
+++ src/test/resources/ECSE429_PartB/ID007_UpdateCategoryDesc.feature
@@ -0,0 +1,36 @@
+Feature: Update a category's description
+  As an user,
+  I want to be able to update a category’s description,
+  so that it is more accurate
+
+  #Normal Flow
+  @scenarioOutline
+  Scenario Outline: Update an existing category with a new title
+    Given an existing category "<existing_category>" with description "<existing_desc>"
+    When I update the existing description to "<new_desc>"
+    Then the expected category status code received from the system is "<statusCode>"
+    And the existing category's description should be "<new_desc>"
+    Examples:
+      | existing_category | existing_desc          | new_desc   | statusCode |
+      | pottery           | clay, glazes and stuff | clay stuff | 200        |
+
+  #Alternate flow
+  @scenarioOutline
+  Scenario Outline: Update an existing category with the same title
+    Given an existing category "<existing_category>" with description "<existing_desc>"
+    When I update the existing description to "<new_desc>"
+    Then the expected category status code received from the system is "<statusCode>"
+    And the existing category's description should be "<new_desc>"
+    Examples:
+      | existing_category | existing_desc          | new_desc               | statusCode |
+      | pottery           | clay, glazes and stuff | clay, glazes and stuff | 200        |
+
+  #Error flow
+  @scenarioOutline
+  Scenario Outline: Update a non existing category with a new category
+    Given an existing category "<existing_category>" with description "<existing_desc>"
+    When I update the description to "<new_desc>" of a category that doesn't exist
+    Then the expected category status code received from the system is "<statusCode>"
+    Examples:
+      | existing_category | existing_desc          | new_desc               | statusCode |
+      | pottery           | clay, glazes and stuff | clay, glazes and stuff | 404        |
